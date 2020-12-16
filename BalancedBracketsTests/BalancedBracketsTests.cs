aa6e6404b00300f0ed4746e782424961ddc75f99
stacey <staceystroh.84@gmail.com>
2020-12-16 12:35:13 -0600

    tests

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -9,9 +9,66 @@ namespace BalancedBracketsTests
         // TODO: Add tests to this file.
 
         [TestMethod]
-        public void EmptyTest()
+        public void OnlyBracketsReturnsTrue()
         {
-            Assert.AreEqual(true, true);
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+
+        [TestMethod]
+        public void ContainsAnyChar()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+
+        [TestMethod]
+        public void StringInBracketReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[launchcode]"));
+        }
+
+        [TestMethod]
+        public void StringAndBracketReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("launch[code]"));
+        }
+
+        [TestMethod]
+        public void StringsAndBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("launchcode[]"));
+        }
+
+        [TestMethod]
+        public void NestedReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[[LaunchCode]]"));
+        }
+
+        [TestMethod]
+        public void OutOfOrderBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+
+        [TestMethod]
+        public void OnlyLeftBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[launchcode"));
+        }
+
+        [TestMethod]
+        public void OnlyRightBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("launchcode]"));
+        }
+
+
+        [TestMethod]
+        public void OpenBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[launch[code]"));
         }
     }
+
+   
 }
